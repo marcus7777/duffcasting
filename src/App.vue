13dde611ad48576000c79188eae18accd4f7afe4
marcus <marcus@hhost.me>
2019-11-19 12:48:09 +0000

    getting there

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,111 @@
     </v-app-bar>
 
     <v-content>
-      <v-carousel cycle hide-delimiters show-arrows-on-hover style="height:80vh;background: #000;" >
-        <v-carousel-item v-for="(slide, i) in images" :key="i" hide-delimiters >
+      <v-carousel cycle hide-delimiters show-arrows-on-hover style="height:170px;background: #000;" >
+        <v-carousel-item v-for="(slide, i) in inSlideShow(cv)" :key="i" hide-delimiters >
           <div style="top:0;white-space: nowrap; background: #000;">
-            <span v-for="(l, k) in images" :key="k" style="background: #fff; border-radius: 1vh; width:7vh; height:7vh; margin:3.5vh;display: inline-block;" >
+            <span v-for="(l, k) in holes" :key="k" style="background: #fff; border-radius: 5px; width:15px; height:15px; margin:11px 15px 0 0 ;display: inline-block;" >
             </span>
           </div>
           <v-sheet>
-            <v-row style="height:calc(59vh - 50px)" align="center" justify="center" >
-             <div class="display-3">{{ slide }} Slide</div>
+            <v-row style="height:100px" align="center" justify="center" >
+             <div class="display-3">{{ slide.name }}</div>
             </v-row>
           </v-sheet>
           <div style="bottom:0;white-space: nowrap; background: #000;">
-            <span v-for="(l, k) in images" :key="k" style="background: #fff; border-radius: 1vh; width:7vh; height:7vh; margin:3.5vh;display: inline-block;" >
+            <span v-for="(l, k) in holes" :key="k" style="background: #fff; border-radius: 5px; width:15px; height:15px; margin:11px 15px 0 0 ;display: inline-block;" >
             </span>
           </div>
         </v-carousel-item>
       </v-carousel>
+
+      <div style="margin:10px"  v-for="(Category, i) in Categories" :key="i" >
+        <h2>{{Category}}</h2>
+        <v-sheet dark>
+          <v-data-table :headers="cvHeaders" :items="cvfilter(cv,Category)" :items-per-page="allImportant(cvfilter(cv,Category))">
+            <template v-slot:item.Link="{ item }">
+              <a class="imdb" v-if="item.Link" :href="item.Link" target="imdb" :title="item.name">IMDb</a>
+            </template>
+          </v-data-table>
+        </v-sheet>
+      </div>
     </v-content>
+    <v-footer  v-bind="localAttrs" padless >
+      <v-card dark flat tile width="100%" class="lighten-1 text-center"
+      >
+        <v-card-text>
+	  <a href="https://www.pushtable.com/dashboard">Content Management</a>
+	  <a href="https://console.firebase.google.com">Hosting</a>
+	  <a href="https://github.com/marcus7777/duffcasting">Source Code</a>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} ~ <strong>Maureen Duff</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
+  methods: {
+    cvfilter(cv, cat) {
+      return cv.reduce((a, p) => {
+        if (p.Categories.replace(/^\d+/g,"") === cat) {
+          a.push(p)
+        }
+        return a
+      }, [])
+    },
+    inSlideShow(cv) {
+      return cv.reduce((a, p) => {
+        if (p.Slide_Show) {
+          a.push(p)
+        }
+        return a
+      }, [])
+    },
+    allImportant(cv) {
+      return cv.reduce((a, p, i) => {
+        if (p.Important) {
+          a = Math.max(a,(i+1))
+        }
+        return a
+      }, 5)
+    },
+  },
   data() {
+    this.getData(data => {
+      this.data = data
+      this.cv = Object.keys(data.cv).map(k => {
+        return data.cv[k]
+      }).sort((a, b) => {
+        return +a.Year < +b.Year
+      })
+      this.Categories = Object.keys(this.cv.reduce((a, p) => {
+        a[p.Categories] = 1
+        return a
+      }, {})).sort().map(p => p.replace(/^\d+/g,""))
+
+    })
     return {
-      images: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32 ]
+      data: {},
+      cv: [],
+      Categories: [],
+      cvHeaders: [
+        {text:"Project",value:"name"},
+        {text:"Director", value:"Director"},
+        {text:"Producer", value:"Producer"},
+        {text:"Actors", value:"Actors"},
+        {text:"", value:"Link"},
+        {text:"",value:"Notes"},
+      ],
+      holes: Array(99),
     }
   },
 };
--- src/main.js
+++ src/main.js
@@ -2,9 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+import VueRouter from 'vue-router'
+import './registerServiceWorker'
+
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+var requestURL = "https://maureenduffcasting-c8651.firebaseio.com/.json"
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+Vue.prototype.getData = function(cb) {
+  if (localStorage.getItem("d")) {
+    cb(JSON.parse(localStorage.getItem("d")))
+  }
+  request.onload = function() {
+    localStorage.setItem("d", JSON.stringify(request.response));
+    cb(request.response)
+  }
+  
+}
+
 
 new Vue({
+  render: h => h(App),
   vuetify,
-  render: h => h(App)
 }).$mount('#app')
+
